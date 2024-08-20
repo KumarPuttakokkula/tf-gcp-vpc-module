c7e5306c7a95717f7247b61c82a3c90a9536adfe
KumarPuttakokkula <50619669+KumarPuttakokkula@users.noreply.github.com>
2024-08-19 23:05:46 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,4 @@
 resource "google_compute_network" "vpc_network" {
-  project                 = var.project_id
-  name                    = var.vpc_name
-  auto_create_subnetworks = true
+  name = "vpc-network"
   network_firewall_policy_enforcement_order = "BEFORE_CLASSIC_FIREWALL"
 }
